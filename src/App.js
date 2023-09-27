6d6fb416d3ecf2129af48f669beda761fdf1876d
Roaa Abu-shaqrah <abushaqrah.roaa@gmail.com>
2023-09-27 21:41:30 +0300

    Booking project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Header from './Component/header';
+import  './Component/header.css';
+
+ class App extends Component {
+  render() {
+    return (
+      <Header/>
+    )
+  }
+}
+export default App;
